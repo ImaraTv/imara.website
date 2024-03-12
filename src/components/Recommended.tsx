03375bc50c45f0b449bbc10c4d0b7ec4055fc6f2
sammymwangangi <sammymwinzi2@gmail.com>
2024-03-12 09:08:52 +0300

    UI fixes

--- src/components/Recommended.tsx
+++ src/components/Recommended.tsx
@@ -367,7 +367,7 @@ export function Recommended() {
                             ))}
                         </ul>
                         <Transition appear show={isOpen} as={Fragment}>
-                            <Dialog as="div" className="relative z-10" onClose={closeModal} selectedItem={selectedItem}>
+                            <Dialog as="div" className="relative z-10" onClose={closeModal}>
                                 <Transition.Child
                                     as={Fragment}
                                     enter="ease-out duration-300"
