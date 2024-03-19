fd15c82c41fe9fcf69aa7da533073c6657bc0984
sammymwangangi <sammymwinzi2@gmail.com>
2024-03-19 10:21:07 +0300

    Update videos page

--- src/app/videos/page.tsx
+++ src/app/videos/page.tsx
@@ -1,7 +1,7 @@
 "use client"
 import { Footer } from '@/components/Footer'
 import { Header } from '@/components/Header'
-import { Fragment, useState } from 'react'
+import React, { Fragment, useEffect, useState } from "react";
 import { Listbox, Dialog, Transition } from '@headlessui/react'
 import { CheckIcon, ChevronDownIcon } from '@heroicons/react/20/solid'
 import { Container } from '@/components/Container'
@@ -111,11 +111,21 @@ const suggestions = [
   },
 
 ]
-const files = [
+
+interface File {
+  title: string;
+  category: string;
+  time: string;
+  description: string;
+  source: string;
+  // Add other properties as needed
+}
+const files: File[] = [
   {
     title: 'Chills for Who',
     category: 'Mental health',
     time: '1hr 23 min',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt.',
     source:
       'https://images.unsplash.com/photo-1582053433976-25c00369fc93?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=512&q=80',
   },
@@ -123,6 +133,7 @@ const files = [
     title: 'The Circle',
     category: 'Politics',
     time: '45 min',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt.',
     source:
       'https://images.unsplash.com/photo-1582053433976-25c00369fc93?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=512&q=80',
   },
@@ -130,6 +141,7 @@ const files = [
     title: 'Hapa nje kwetu',
     category: 'Mental health',
     time: '45 min',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt.',
     source:
       'https://images.unsplash.com/photo-1582053433976-25c00369fc93?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=512&q=80',
   },
@@ -137,6 +149,7 @@ const files = [
     title: 'Against my will short film',
     category: 'GBV',
     time: '22 min',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt.',
     source:
       'https://images.unsplash.com/photo-1582053433976-25c00369fc93?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=512&q=80',
   },
@@ -144,6 +157,7 @@ const files = [
     title: 'Making the right decisions',
     category: 'GBV',
     time: '22 min',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt.',
     source:
       'https://images.unsplash.com/photo-1582053433976-25c00369fc93?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=512&q=80',
   },
@@ -151,6 +165,7 @@ const files = [
     title: 'Cut across my will',
     category: 'GBV',
     time: '22 min',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt.',
     source:
       'https://images.unsplash.com/photo-1582053433976-25c00369fc93?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=512&q=80',
   },
@@ -158,13 +173,15 @@ const files = [
     title: 'Covid - Comic cartoons',
     category: 'GBV',
     time: '22 min',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt.',
     source:
       'https://images.unsplash.com/photo-1582053433976-25c00369fc93?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=512&q=80',
   },
   {
-    title: 'Chills for Who',
+    title: 'Chills for Who2',
     category: 'Mental health',
     time: '1hr 23 min',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt.',
     source:
       'https://images.unsplash.com/photo-1582053433976-25c00369fc93?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=512&q=80',
   },
@@ -172,6 +189,7 @@ const files = [
     title: 'Life of Wambo',
     category: 'FGM',
     time: '45 min',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt.',
     source:
       'https://images.unsplash.com/photo-1582053433976-25c00369fc93?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=512&q=80',
   },
@@ -181,24 +199,35 @@ export default function Videos() {
   const [selected, setSelected] = useState(qualities[0])
   const [active, setActive] = useState(dates[0])
 
-  let [isOpen, setIsOpen] = useState(true)
+  let [isOpen, setIsOpen] = useState(false)
 
-  function closeModal() {
-    setIsOpen(false)
-  }
+  const [selectedItem, setSelectedItem] = useState(null);
+  const [selectedFile, setSelectedFile] = useState<File | null>(null);
+  const openModal = (file: File) => {
+    setSelectedFile(file);
+    setIsOpen(true);
+  };
 
-  function openModal() {
-    setIsOpen(true)
-  }
+  const closeModal = () => {
+    setIsOpen(false);
+    setSelectedFile(null);
+  };
+
+  useEffect(() => {
+    // Reset isOpen state when the component unmounts
+    return () => {
+      setIsOpen(false);
+    };
+  }, []);
   return (
     <>
       <Header />
       <main>
         <Container>
-          <div className='font-bold text-[40px] text-[#2B2B2B] mt-14'>All our videos</div>
+          <div className='font-bold text-[20px] md:text-[40px] text-[#2B2B2B] mt-5 md:mt-14'>All our videos</div>
 
-          <div className='flex justify-between mt-[53px]'>
-            <div className='flex px-6'>
+          <div className='flex flex-col md:flex-row gap-2 justify-between mt-[40px] md:mt-[53px]'>
+            <div className='flex'>
               <div className="hidden md:flex flex-1 items-center justify-center px-2">
                 <div className="w-full max-w-lg lg:max-w-xs">
                   <label htmlFor="search" className="sr-only">
@@ -222,17 +251,17 @@ export default function Videos() {
                 <button
                   type="button"
                   key={category.id}
-                  className="inline-flex mr-2 items-center gap-x-2 rounded-md bg-white px-6 py-2 text-[17px] font-medium text-[#525252] shadow-sm ring-1 ring-inset ring-[#525252] hover:bg-gray-50"
+                  className="inline-flex mr-2 items-center gap-x-2 rounded-md bg-white px-6 py-2 text-xs md:text-[17px] font-medium text-[#525252] shadow-sm ring-1 ring-inset ring-[#525252] hover:bg-gray-50"
                 >
                   {category.name}
                 </button>
               ))}
             </div>
-            <div className='flex px-6'>
+            <div className='flex'>
               <div className="">
                 <Listbox value={selected} onChange={setSelected}>
                   <div className="relative mt-1">
-                    <Listbox.Button className="relative inline-flex mr-2 items-center gap-x-2 rounded-md bg-white px-6 py-2 text-[17px] font-medium text-[#525252] shadow-sm ring-1 ring-inset ring-[#525252] hover:bg-gray-50">
+                    <Listbox.Button className="relative inline-flex mr-2 items-center gap-x-2 rounded-md bg-white px-6 py-2 text-xs md:text-[17px] font-medium text-[#525252] shadow-sm ring-1 ring-inset ring-[#525252] hover:bg-gray-50">
                       <span className="block truncate pr-1">{selected.name}</span>
                       <span className="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2">
                         <ChevronDownIcon
@@ -282,7 +311,7 @@ export default function Videos() {
               <div className="">
                 <Listbox value={active} onChange={setActive}>
                   <div className="relative mt-1">
-                    <Listbox.Button className="relative inline-flex mr-2 items-center gap-x-2 rounded-md bg-white px-6 py-2 text-[17px] font-medium text-[#525252] shadow-sm ring-1 ring-inset ring-[#525252] hover:bg-gray-50">
+                    <Listbox.Button className="relative inline-flex mr-2 items-center gap-x-2 rounded-md bg-white px-6 py-2 text-xs md:text-[17px] font-medium text-[#525252] shadow-sm ring-1 ring-inset ring-[#525252] hover:bg-gray-50">
                       <span className="block truncate pr-1">{active.name}</span>
                       <span className="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2">
                         <ChevronDownIcon
@@ -297,7 +326,7 @@ export default function Videos() {
                       leaveFrom="opacity-100"
                       leaveTo="opacity-0"
                     >
-                      <Listbox.Options className="absolute mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black/5 focus:outline-none sm:text-sm">
+                      <Listbox.Options className="absolute mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-xs md:text-base shadow-lg ring-1 ring-black/5 focus:outline-none">
                         {dates.map((date, dateIdx) => (
                           <Listbox.Option
                             key={dateIdx}
@@ -338,30 +367,30 @@ export default function Videos() {
 
           </div>
 
-          <div className='flex justify-between mt-[90px]'>
+          <div className='flex flex-col md:flex-row justify-between mt-[90px]'>
             <div>
-              <ul role="list" className="grid grid-cols-2 gap-x-4 gap-y-[100px] sm:grid-cols-3 sm:gap-x-6 lg:grid-cols-3 xl:gap-x-8">
+              <ul role="list"
+                className="grid grid-cols-2 gap-x-4 gap-y-[25px] md:gap-y-[100px] sm:grid-cols-3 sm:gap-x-6 lg:grid-cols-2 xl:gap-x-8">
                 {files.map((file) => (
                   <li key={file.title} className="relative">
-                    <div onClick={openModal} className="group aspect-h-7 aspect-w-10 block w-full overflow-hidden rounded-lg bg-gray-100 focus-within:ring-2 focus-within:ring-indigo-500 focus-within:ring-offset-2 focus-within:ring-offset-gray-100">
-                      <img src={file.source} alt="" className="pointer-events-none object-cover group-hover:opacity-75" />
-
-                      <button type="button" className="absolute inset-0 focus:outline-none">
-                        <span className="sr-only">View details for {file.title}</span>
-                      </button>
+                    <div onClick={() => openModal(file)}
+                      className="relative group aspect-h-7 aspect-w-10 block w-full overflow-hidden rounded-lg bg-gray-100 focus-within:ring-2 focus-within:ring-indigo-500 focus-within:ring-offset-2 focus-within:ring-offset-gray-100">
+                      <img src={file.source} alt=""
+                        className="pointer-events-none h-full w-full object-cover group-hover:opacity-75" />
+                      <Image className='w-[32.81px] md:w-[61px] h-[23.13px] md:h-[43px] absolute inset-0 m-auto' width={50} height={43} src={Yt}
+                        alt={"ÿt"} />
                     </div>
-                    <Image className='absolute inset-0 top-1/4 left-1/2' width={50} height={43} src={Yt} alt={"ÿt"} />
-                    <div className='flex gap-3 mt-5'>
+                    <div className='flex gap-3 mt-[18px] md:mt-5'>
                       <button
                         type="button"
-                        className="inline-flex items-center gap-x-2 rounded-md bg-white px-6 py-2.5 text-[17px] font-medium text-[#525252] shadow-sm ring-1 ring-inset ring-[#007BFF] hover:bg-gray-50"
+                        className="inline-flex items-center gap-x-2 rounded-md bg-white px-2 md:px-6 py-1.5 text-[12px] md:text-[17px] font-medium text-[#525252] shadow-sm ring-1 ring-inset ring-[#007BFF] hover:bg-gray-50"
                       >
                         {file.time}
                       </button>
-                      <p className="pointer-events-none mt-2 block truncate text-[16px] font-medium text-[#525252]">{file.category}</p>
+                      <p className="pointer-events-none mt-2 block truncate text-[12px] md:text-[16px] font-medium text-[#525252]">{file.category}</p>
                     </div>
 
-                    <p className="pointer-events-none block text-[19px] mt-9 font-bold text-[#525252]">{file.title}</p>
+                    <p className="pointer-events-none block text-[15px] md:text-[19px] mt-4 md:mt-9 font-bold text-[#525252]">{file.title}</p>
                   </li>
                 ))}
               </ul>
@@ -390,25 +419,28 @@ export default function Videos() {
                         leaveFrom="opacity-100 scale-100"
                         leaveTo="opacity-0 scale-95"
                       >
-                        <Dialog.Panel className="w-[342px] h-auto max-w-md transform overflow-hidden rounded-2xl bg-white p-6 text-left align-middle shadow-xl transition-all">
+                        <Dialog.Panel
+                          className="w-[342px] h-auto max-w-md transform overflow-hidden rounded-2xl bg-white p-6 text-left align-middle shadow-xl transition-all">
                           <Dialog.Title
                             as="h3"
                             className="text-[19px] font-bold text-[#525252]"
                           >
-                            Making the right decision
+                            {selectedFile && selectedFile.title}
                           </Dialog.Title>
                           <div className='flex gap-7 items-center mt-5'>
-                            <button className='px-2 py-1 bg-[#F2970F] rounded-lg font-bold text-[18px] text-white'>HD</button>
-                            <div className='font-medium text-[15px] text-[#525252]'>2023</div>
-                            <div className='font-medium text-[15px] text-[#525252]'>1 hr </div>
+                            <button
+                              className='px-2 py-1 bg-[#F2970F] rounded-lg font-bold text-[18px] text-white'>HD
+                            </button>
+                            <div className='font-medium text-[15px] text-[#525252]'>{selectedFile && selectedFile.time}</div>
                           </div>
                           <div className='flex gap-1 items-center mt-[55px] mb-[27px]'>
-                            <div className='font-bold text-[20px] text-[#525252]'>Category:</div>
-                            <div className='font-medium text-[17px] text-[#525252]'>GBV awareness</div>
+                            <div className='font-bold text-[20px] text-[#525252]'>Category:
+                            </div>
+                            <div className='font-medium text-[17px] text-[#525252]'>{selectedFile && selectedFile.category}</div>
                           </div>
                           <div className="mt-2">
                             <p className="text-[15px] text-[#525252]">
-                              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt.
+                              {selectedFile && selectedFile.description}
                             </p>
                           </div>
 
@@ -427,9 +459,9 @@ export default function Videos() {
               </Transition>
             </div>
             <div>
-              <ul role="list" className="-mt-12 space-y-12 xl:col-span-3">
+              <ul role="list" className="mt-10 md:-mt-12 space-y-12 xl:col-span-3">
                 {suggestions.map((suggestion) => (
-                  <li key={suggestion.name} className="flex flex-col gap-10 pt-12 sm:flex-row items-center justify-center">
+                  <li key={suggestion.id} className="flex flex-row md:flex-col gap-10 pt-12 sm:flex-row items-center justify-center">
                     <img className="aspect-[4/5] w-[131px] flex-none rounded-l-2xl object-cover" src={suggestion.imageUrl} alt="" />
                     <div className="max-w-xl flex-auto space-y-[26px]">
                       <p className="text-[17px] font-medium text-[#525252]">{suggestion.series}</p>
@@ -446,8 +478,6 @@ export default function Videos() {
             <div className='flex items-center justify-center p-4 bg-[#0000002E] font-semibold text-[15px] text-white'>3</div>
             <div className='flex items-center justify-center p-4 bg-[#0000002E] font-semibold text-[15px] text-white'>4</div>
             <div className='flex items-center justify-center p-4 bg-[#0000002E] font-semibold text-[15px] text-white'>5</div>
-            <div className='flex items-center justify-center p-4 bg-[#0000002E] font-semibold text-[15px] text-white'>6</div>
-            <div className='flex items-center justify-center p-4 bg-[#0000002E] font-semibold text-[15px] text-white'>7</div>
           </div>
 
 
