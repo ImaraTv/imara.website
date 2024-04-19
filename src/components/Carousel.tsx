fe73cedcfdada9c0d3cf04d2ccee5498738bc3fd
Sammy <sammymwinzi2@gmail.com>
2024-04-19 12:38:16 +0300

    Added Bookmarks

--- src/components/Carousel.tsx
+++ src/components/Carousel.tsx
@@ -13,6 +13,7 @@ import {Pagination, Navigation, Autoplay} from 'swiper/modules';
 import { css } from '@emotion/react';
 import Slide1 from "@/images/carousel.png"
 import Link from "next/link";
+import BookmarkButton from '@/components/BookmarkButton';
 
 const items = [
     {
@@ -121,20 +122,7 @@ export function CarouselHome() {
                           >
                               Watch Now
                           </Link>
-                          <Link
-                            href="/sign-in"
-                            className="group inline-flex gap-3 items-center justify-center rounded-lg py-2 px-10 text-[12px] md:text-[17px] font-medium text-white focus:outline-none"
-                          >
-
-                              <svg width="24" height="24" viewBox="0 0 24 24" fill="none"
-                                   xmlns="http://www.w3.org/2000/svg">
-                                  <path
-                                    d="M4 2H20C20.5523 2 21 2.44772 21 3V22.2763C21 22.5525 20.7761 22.7764 20.5 22.7764C20.4298 22.7764 20.3604 22.7615 20.2963 22.7329L12 19.0313L3.70373 22.7329C3.45155 22.8455 3.15591 22.7322 3.04339 22.4801C3.01478 22.4159 3 22.3465 3 22.2763V3C3 2.44772 3.44772 2 4 2ZM12 13.5L14.9389 15.0451L14.3776 11.7725L16.7553 9.45492L13.4695 8.97746L12 6L10.5305 8.97746L7.24472 9.45492L9.62236 11.7725L9.06107 15.0451L12 13.5Z"
-                                    fill="white" />
-                              </svg>
-
-                              <span>Save</span>
-                          </Link>
+                          <BookmarkButton videoId={item.id} />
                       </div>
                   </div>
               </SwiperSlide>
