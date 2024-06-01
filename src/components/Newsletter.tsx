2f3a6f9b6c58454da50f2e3b2e8f0658bf6a3850
Gianna Makdesi <66941829+G-anna89@users.noreply.github.com>
2024-06-01 07:37:52 -0700

    Update Newsletter.tsx
    resolve newsletter subscription bug

--- src/components/Newsletter.tsx
+++ src/components/Newsletter.tsx
@@ -54,7 +54,7 @@ export function Newsletter() {
                             type="email"
                             autoComplete="email"
                             required
-                            className="min-w-0 flex-auto rounded-md border-0 bg-white/5 px-3.5 py-2 text-white shadow-sm ring-1 ring-inset ring-[#3F4C5373] focus:ring-2 focus:ring-inset focus:ring-[#3F4C5373] sm:text-sm sm:leading-6"
+                            className="min-w-0 flex-auto rounded-md border-0 bg-white/5 px-3.5 py-2 text-black shadow-sm ring-1 ring-inset ring-[#3F4C5373] focus:ring-2 focus:ring-inset focus:ring-[#3F4C5373] sm:text-sm sm:leading-6"
                             placeholder="Email"
                             value={formData['email']}
                             onChange={handleChange}
