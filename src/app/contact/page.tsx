7d025d058ac21675b81a922df2af2572c9854488
renjeroham <renjeroham.vargas@gmail.com>
2024-08-18 14:36:45 -0400

    Update page.tsx

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -147,7 +147,7 @@ export default function Contact() {
                                             type="email"
                                             name="email"
                                             id="email"
-                                            placeholder='Phone number or Email'
+                                            placeholder='Email'
                                             autoComplete="email"
                                             className="block w-full bg-transparent rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                                             value={formData['email']}
