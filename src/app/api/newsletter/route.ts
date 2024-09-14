3ec6e9e1427113d5b248631b5f40c4cf592c68f4
sammymwangangi <sammymwinzi2@gmail.com>
2024-09-14 11:24:41 +0300

    Newsletter now working

--- src/app/api/newsletter/route.ts
+++ src/app/api/newsletter/route.ts
@@ -1,34 +1,45 @@
-// Example: pages/api/newsletter.js or newsletter.ts
+import type { NextApiResponse } from 'next';
+import { NextResponse } from 'next/server';
 
-import type { NextApiRequest, NextApiResponse } from 'next';
-const mailchimp = require('@mailchimp/mailchimp_marketing');
-
-mailchimp.setConfig({
-  apiKey: process.env.MAILCHIMP_API_KEY,
-  server: process.env.MAILCHIMP_SERVER_PREFIX,
-});
-
-export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-  if (req.method === 'POST') {  // Ensure POST method is allowed
-    const { email } = req.body;
+export async function POST(req: Request, res: NextApiResponse) {
+  try {
+    const { email } = await req.json();
 
     if (!email) {
       return res.status(400).json({ error: 'Email is required' });
     }
 
-    try {
-      const response = await mailchimp.lists.addListMember(process.env.MAILCHIMP_AUDIENCE_ID, {
+    const MailchimpKey = process.env.MAILCHIMP_API_KEY;
+    const MailchimpServer = process.env.MAILCHIMP_SERVER_PREFIX;
+    const MailchimpAudience = process.env.MAILCHIMP_AUDIENCE_ID;
+
+    if (!MailchimpKey || !MailchimpServer || !MailchimpAudience) {
+      throw new Error('Missing Mailchimp environment variables');
+    }
+
+    const customUrl = `https://${MailchimpServer}.api.mailchimp.com/3.0/lists/${MailchimpAudience}/members`;
+
+    const response = await fetch(customUrl, {
+      method: 'POST',
+      headers: {
+        Authorization: `Basic ${Buffer.from(`anystring:${MailchimpKey}`).toString('base64')}`,
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
         email_address: email,
         status: 'subscribed',
-      });
+      }),
+    });
 
-      return res.status(201).json({ message: 'Subscription successful' });
-    } catch (error) {
-      console.error('Mailchimp error:', error);
-      return res.status(500).json({ error: 'Subscription failed' });
+    if (!response.ok) {
+      const errorData = await response.json();
+      return res.status(response.status).json({ error: errorData.detail });
     }
-  } else {
-    res.setHeader('Allow', ['POST']);  // Allow only POST requests
-    res.status(405).end(`Method ${req.method} Not Allowed`);  // Respond with 405 for other methods
+
+    const received = await response.json();
+    return NextResponse.json(received);
+  } catch (error) {
+    console.error('Error:', error);
+    return res.status(500).json({ error: 'Internal Server Error' });
   }
 }
