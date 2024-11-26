0c838f9fe0405aee4f5ac1f580f494f2b398e72f
sammymwangangi <sammymwinzi2@gmail.com>
2024-11-26 15:28:27 +0300

    Updating PWA

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,15 +1,19 @@
 import { Roboto } from 'next/font/google'
-import { Ubuntu } from 'next/font/google'
-import { Montserrat } from 'next/font/google'
 import clsx from 'clsx'
 import '@/styles/tailwind.css'
 import '@/styles/global.css'
-import { type Metadata } from 'next'
+import { type Metadata, type Viewport } from 'next'
 import React from "react";
 import { Suspense } from 'react'
 import CookieConsentComponent from '@/components/CookieConsent';
-import { GoogleAnalytics } from '@next/third-parties/google';
-import Head from 'next/head'
+import { GoogleAnalytics } from '@next/third-parties/google'
+
+export const viewport: Viewport = {
+  width: 'device-width',
+  initialScale: 1,
+  maximumScale: 1,
+  themeColor: "#000000",
+}
 
 export const metadata: Metadata = {
   title: {
@@ -19,9 +23,20 @@ export const metadata: Metadata = {
   description: 'Bridging Entertainment & Education.',
   manifest: '/manifest.json',
   icons: {
-    apple: [
+    icon: [
+      { url: '/favicon-48x48.png', sizes: '48x48', type: 'image/png' },
+      { url: '/favicon.ico', sizes: 'any' },
+      { url: '/favicon.svg', type: 'image/svg+xml' },
       { url: '/icon-192x192.png', sizes: '192x192', type: 'image/png' },
       { url: '/icon-512x512.png', sizes: '512x512', type: 'image/png' },
+    ],
+    apple: [
+      { url: '/apple-touch-icon.png', sizes: '180x180', type: 'image/png' },
+      { url: '/icon-192x192.png', sizes: '192x192', type: 'image/png' },
+      { url: '/icon-512x512.png', sizes: '512x512', type: 'image/png' },
+    ],
+    shortcut: [
+      { url: '/favicon.ico' }
     ],
   },
   appleWebApp: {
@@ -34,30 +49,25 @@ export const metadata: Metadata = {
     telephone: false,
   },
   themeColor: '#000000',
-  viewport: {
-    width: 'device-width',
-    initialScale: 1,
-    maximumScale: 1,
-  },
+  other: {
+    'mobile-web-app-capable': 'yes',
+    'apple-mobile-web-app-capable': 'yes',
+    'application-name': 'ImaraTv',
+    'apple-mobile-web-app-title': 'ImaraTv',
+    'theme-color': '#000000',
+    'msapplication-navbutton-color': '#000000',
+    'apple-mobile-web-app-status-bar-style': 'black-translucent',
+    'msapplication-starturl': '/',
+  }
 }
 
-const ubuntu = Ubuntu({
-  weight: ["300","400","500","700"],
-  subsets: ['latin'],
-  display: 'swap',
-})
-const montserrat = Montserrat({
-  weight: ["300","400","500","600","700"],
-  subsets: ['latin'],
-  display: 'swap',
-})
+
 const roboto = Roboto({
   weight: ["100","300","400","500","700","900"],
     style: ["normal" ,"italic"],
     subsets: ['latin'],
     display: 'swap',
 })
-
 
 export default function RootLayout({
   children,
@@ -72,23 +82,16 @@ export default function RootLayout({
         roboto.className,
       )}
     >
-      <Head>
-        <link rel="icon" type="image/png" href="/favicon-48x48.png" sizes="48x48" />
-        <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
-        <link rel="shortcut icon" href="/favicon.ico" />
-        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
-        <meta name="apple-mobile-web-app-title" content="ImaraTv" />
-        <link rel="manifest" href="/site.webmanifest" />
-      </Head>
       <body className={clsx(
         'flex h-full flex-col',
         roboto.className,
       )}>
-      
-        <Suspense fallback={<>Loading...</>}>{children}</Suspense>
-        <CookieConsentComponent />
+        <Suspense fallback={null}>
+          <CookieConsentComponent />
+        </Suspense>
+        {children}
+        <GoogleAnalytics gaId="G-WZXW1066ZR" />
       </body>
-      <GoogleAnalytics gaId="G-WZXW1066ZR" />
     </html>
   )
 }
